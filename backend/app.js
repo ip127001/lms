4bab6a8fe3288a90c52e6243208c3e2df370aad0
Rohit Kumawat <rkumawat125@gmail.com>
2019-04-14 21:26:09 +0530

    initialized the node.js backend

---
+++ backend/app.js
@@ -0,0 +1,41 @@
+const path = require('path')
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+
+const bookRoutes = require('./routes/book');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();;
+});
+
+app.use('/book', bookRoutes);
+app.use('/auth', authRoutes);
+
+app.use((error, req, res, next) => {
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+    res.status().json({message: message, data: data});
+});
+
+mongoose.connect('mongodb+srv://rk:9pnwE86CrIBYXaWH@ecommerce-rs4wl.mongodb.net/messages?retryWrites=true')
+    .then(result => {
+        app.listen(8080);
+    })
+    .catch(err => {
+        console.log(err);
+    })
