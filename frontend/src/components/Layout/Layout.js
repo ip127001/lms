3bb8728214223a0e7d4374be0f9db722e8da63ad
Rohit Kumawat <rkumawat125@gmail.com>
2019-04-16 20:14:30 +0530

    added Layout Component css

--- frontend/src/components/Layout/Layout.js
+++ frontend/src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/Aux/Aux';
 
+import classes from './Layout.css';
+
 const layout = (props) => (
     <Aux>
         <div>toolbar, sidedrawer, backdrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
