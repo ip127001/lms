b9b7a92ff6b9e892dda25f56a869004b180e45a6
Rohit Kumawat <rkumawat125@gmail.com>
2019-04-26 12:05:56 +0530

    Button Component updated with props

--- frontend/src/components/UI/Button/Button.js
+++ frontend/src/components/UI/Button/Button.js
@@ -5,7 +5,8 @@ import classes from './Button.css';
 const Button = (props) => (
     <button 
         className={[classes.Button, classes[props.btnType]].join(' ')}
-        onClick={props.clicked}>{props.children}</button>
+        onClick={props.clicked}
+        disabled={props.disabled}>{props.children}</button>
 );
 
 export default Button;
