d20765f656da30dc44b47362ed13ded38152309e
Rohit Kumawat <rkumawat125@gmail.com>
2019-05-03 05:30:33 +0530

    studentBooks component created and embedded in container fro showing list of books

--- frontend/src/components/Student/StudentBooks/StudentBooks.js
+++ frontend/src/components/Student/StudentBooks/StudentBooks.js
@@ -1,9 +1,45 @@
 import React from 'react';
 
+import classes from './studentBooks.css';
+
 const studentBooks = (props) => {
+    console.log('props', props.books);
+    let tableContent;
+    if(props.books) {
+        tableContent = props.books.map(book => (
+            <tr key={book.bookId}>
+                <td>{book.bookName}</td>
+                <td>{book.bookId}</td>
+                <td>{book.fine}</td>
+                <td>{book.reIssueDate}</td>
+            </tr>
+        ));
+    } else {
+        tableContent = (
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        )
+    }
     return (
         <div>
-            <h1> here are the books and associated fine</h1>
+            <h6 style={{textAlign: "center"}}> here are the books and associated fine</h6>
+            <table className={classes.Customers}>
+                <thead>
+                    <tr>
+                        <th>Book Name</th>
+                        <th>Book ID</th>
+                        <th>Fine</th>
+                        <th>Date of ReIssue</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tableContent}
+                </tbody>
+            </table>
         </div>
     );
 }
