49b74c2719b645b7b6333443fb83aba0681bc9b5
Rohit Kumawat <rkumawat125@gmail.com>
2019-05-03 17:44:55 +0530

    added select input data also acc to props value

--- frontend/src/components/Student/StudentSearch/StudentSearch.js
+++ frontend/src/components/Student/StudentSearch/StudentSearch.js
@@ -5,16 +5,29 @@ import Button from '../../UI/Button/Button';
 
 const studentSearch = (props) => {
     let inputClass = [classes.InputElement];
+    let options = null;
     if (props.touched && props.invalid) {
         inputClass.push(classes.Invalid);
     }
+    if (props.select) {
+        options = <div className={classes.Select}>
+            <select
+                    className={inputClass.join(' ')}
+                    onChange={props.selectChanged} >
+                    <option key="1" value="tag">tag</option>
+                    <option key="2" value="subject">subject</option>
+                    <option key="3" value="name">name</option>
+                    <option key="4" value="author">author</option>
+                </select>
+        </div>
+    }
     return (
             <form onSubmit={props.searchStudentHandler} className={classes.StudentSearch}>
-                <div className={classes.Label}>
-                    <label htmlFor="search">Enter Roll no of student:</label>
+                <div className={props.select ? classes.SelectLabel : classes.Label}>
+                    <label htmlFor="search">{props.label}</label>
                 </div>
 
-                <div className={classes.Input}>
+                <div className={props.select ? classes.SelectInput : classes.Input}>
                     <input className={inputClass.join(' ')} 
                             type="text" 
                             name="search" 
@@ -23,8 +36,11 @@ const studentSearch = (props) => {
                             value={props.value}
                             onChange={props.changed} />
                 </div>
-    
-                <div className={classes.Button}>
+                
+
+                {options}
+            
+                <div className={props.select ? classes.SelectButton : classes.Button}>
                     <Button btnType="Success">Search</Button>
                 </div>
             </form>
