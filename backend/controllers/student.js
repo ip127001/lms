26a898687712413f8b8b12c909f7687243d59be6
Rohit Kumawat <rkumawat125@gmail.com>
2019-05-03 05:33:14 +0530

    student controller completed for studentInfo & bookInfo post APIs

---
+++ backend/controllers/student.js
@@ -0,0 +1,126 @@
+const Student = require('../models/student');
+const Book = require('../models/book');
+
+exports.getStudentInfo = (req, res, next) => {
+    console.log('search data', req.body);
+    const search = req.body.search;
+    Student.findOne({roll: search})
+        .then(result => {
+            res.status(201).json({
+                message: 'student information found successfully',
+                studentInfo: result
+            });
+        })
+        .catch(err => {
+            if (!err.statusCode) {  
+            err.statusCode = 500;
+            }
+            next(err);
+        });
+}
+
+exports.postStudentInfo = (req, res, next) => {    
+    console.log('postStudentInfo', req.body);
+
+    const issueDate = req.body.dateOfIssue;
+    const name = req.body.name;
+    const rollNumber = req.body.roll;
+    const semester = req.body.semester;
+    const bookId = req.body.bookId;
+    const branch = req.body.branch;
+    const totalFine = 5;
+    
+    Book.findOne({id: bookId})
+        .then(book => {
+            if(book) {
+                if(book.isAvailable) {
+                    book.isAvailable = false;
+                    book.reissueDate = issueDate;
+                    book.save().then(book => {
+                        const bookName = book.author;
+                        Student.findOne({roll: rollNumber})
+                            .then(student => {
+                                let newArray = student.books;
+                                newArray.push({
+                                    "bookName": bookName,
+                                    "bookId": bookId,
+                                    "reIssueDate": issueDate,
+                                    "fine": 0
+                                })
+                                student.name = name;
+                                student.roll = rollNumber;
+                                student.semester = semester;
+                                student.branch = branch;
+                                student.books = newArray;
+                                student.totalFine = totalFine;
+                                return student.save();
+                            })
+                            .then(result => {
+                                res.status(201).json({
+                                    message: 'student data updated successfully!',
+                                    student: result
+                                });
+                            })
+                            .catch(err => {
+                                if (!err.statusCode) {  
+                                err.statusCode = 500;
+                                }
+                                next(err);
+                            });
+                    }) 
+                    .catch(err => {
+                        console.log(err);
+                    })
+                } else {
+                    res.status(403).json({
+                        message: 'book already issued to someone else'
+                    });
+                }
+            } else {
+                res.status(404).json({
+                    message: 'book not found'
+                });
+            }
+        })
+        .catch(err => {
+            if (!err.statusCode) {  
+                err.statusCode = 500;
+                }
+            next(err);
+        });
+}
+
+
+
+exports.postBookinfo = (req, res, next) => {
+    console.log(req.body);
+    const name = req.body.name;
+    const subject = req.body.subject;
+    const id = req.body.id;
+    const author = req.body.author;
+    const fine = req.body.fine;
+    const tag = req.body.tag;
+
+    const book = new Book({
+        name: name,
+        subject: subject,
+        id: id,
+        author: author,
+        fine: fine,
+        tag: tag
+    });
+
+    book.save()
+        .then(result => {
+            res.status(200).json({
+                message: 'book added successfully',
+                result: result
+            })
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+            err.statusCode = 500;
+            }
+            next(err);
+        });
+}
