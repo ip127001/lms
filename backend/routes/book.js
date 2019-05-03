23c5da2cadaff4cdd1b5710e67e1f1eea1afb82c
Rohit Kumawat <rkumawat125@gmail.com>
2019-05-03 17:44:00 +0530

    added book search route

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const studentController = require('../controllers/student');
+const bookController = require('../controllers/book');
 
 const router = express.Router();
 
@@ -9,6 +10,8 @@ router.post('/students', studentController.postStudentInfo);
 router.post('/search', studentController.getStudentInfo);
 
 router.post('/newBook', studentController.postBookinfo);
+
+router.post('/books', bookController.getBookInfo);
 
 module.exports = router;
 
