7c32d82ea0417cd5828afb88be8a4631e8ed95d6
Rohit Kumawat <rkumawat125@gmail.com>
2019-05-03 05:37:00 +0530

    new path added for books search

--- frontend/src/hoc/App.js
+++ frontend/src/hoc/App.js
@@ -3,7 +3,8 @@ import { Route, Switch } from 'react-router-dom';
 
 import Layout from './Layout/Layout';
 import IssueBooks from '../containers/IssueBooks/IssueBooks';
-import StudentInfo from '../containers/StudentInfo/StudentInfo';
+import RegisterBook from '../containers/RegisterBook/RegisterBook';
+import BooksSearch from '../containers/BooksSearch/BooksSearch';
 
 class App extends Component {
   render() {
@@ -12,7 +13,8 @@ class App extends Component {
         <Layout>
           <Switch>
             <Route path="/" exact component={IssueBooks} />
-            <Route path="/student" component={StudentInfo} />
+            <Route path="/student" component={RegisterBook} />
+            <Route path="/books" component={BooksSearch} />
           </Switch>
         </Layout>
       </div>
