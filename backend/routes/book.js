13b437724366cf8ae84ed5165e08614549b13588
Rohit Kumawat <rkumawat125@gmail.com>
2019-05-06 09:40:15 +0530

    authorization middleware added for route authentication

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -2,12 +2,12 @@ const express = require('express');
 
 const studentController = require('../controllers/student');
 const bookController = require('../controllers/book');
-
+const isAuth = require('../middleware/is-auth');
 const router = express.Router();
 
 router.post('/students', studentController.postStudentInfo);
 
-router.post('/search', studentController.getStudentInfo);
+router.post('/search', isAuth, studentController.getStudentInfo);
 
 router.post('/newBook', studentController.postBookinfo);
 
