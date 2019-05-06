1d928c575da4196ca1ae3ae2a2537570421be7e1
Rohit Kumawat <rkumawat125@gmail.com>
2019-05-06 09:38:35 +0530

    Authorization header allowed in the backend

--- backend/app.js
+++ backend/app.js
@@ -41,7 +41,7 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, application/json');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();;
 });
 
