b4d894e6836e151882b1f74b87ceabe8b97fc85d
Rohit Kumawat <rkumawat125@gmail.com>
2019-05-06 15:07:27 +0530

    controller for adding subAdmins completed

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -37,6 +37,41 @@ exports.signup = (req, res, next) => {
         });
 }
 
+exports.adminSignup = (req, res, next) => {
+    console.log('backend controller');
+    console.log(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const error = new Error('validation failed');
+        error.status = 422;
+        error.data = errors.array();
+        throw error;
+    }
+    const email = req.body.email;
+    const name = req.body.name;
+    const password = req.body.password;
+    bcrypt.hash(password, 12)
+        .then(hashedPw => {
+            const user = new User({
+                email: email,
+                password: hashedPw,
+                name: name,
+                isUser: false
+            });
+            return user.save();
+        })
+        .then(result => {
+            console.log('result', result);
+            res.status(200).json({message: 'user Created', userId: result._id})
+        })
+        .catch(err => {
+            if (!error.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err)
+        });
+}
+
 exports.login = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
@@ -58,13 +93,12 @@ exports.login = (req, res, next) => {
             }
             const token = jwt.sign({
                 email: loadedUser.email,
-                userId: loadedUser._id.toString(),
-                isUser: loadedUser.isUser
+                userId: loadedUser._id.toString()
                 },
                 'secrethosktahai',
                 { expiresIn: '1h' }
             );
-            res.status(200).json({token: token, userId: loadedUser._id.toString()})
+            res.status(200).json({token: token, userId: loadedUser._id.toString(), isUser: loadedUser.isUser})
         })
         .catch(err => {
             if (!err.statusCode) {
