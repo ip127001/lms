2e569b9165c19313a2978bb4e0dfcf8fe1474ebf
Rohit Kumawat <rkumawat125@gmail.com>
2019-05-06 09:29:58 +0530

    interceptors and headers added in axios global file

--- frontend/src/axios-library.js
+++ frontend/src/axios-library.js
@@ -4,4 +4,19 @@ const instance = axios.create({
     baseURL: 'http://localhost:8080/'
 }); 
 
+instance.defaults.headers.common['Content-Type'] = 'application/json';
+
+instance.interceptors.request.use(request => {
+    console.log(request);
+    return request;
+}, error => {
+    console.log(error);
+})
+
+instance.interceptors.response.use(response => {
+    console.log(response);
+    return response;
+}, error => {
+    console.log(error);
+})
 export default instance;
