da1ea051b861373182a8ce48fdd18764b12b1aab
Rohit Kumawat <rkumawat125@gmail.com>
2019-05-06 15:11:03 +0530

    navigationItems settled for admin/student/non-Auth users

--- frontend/src/components/Navigation/NavigationItems/NavigationItems.js
+++ frontend/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -3,13 +3,20 @@ import React from 'react';
 import classes from './NavigationItems.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
-const navigationItems = (props) => (
-    <ul className={classes.NavigationItems}>
-        {props.isAuth ? <NavigationItem link="/" exact>IssueBooks</NavigationItem> : <NavigationItem link="/" exact>Login</NavigationItem>}
-        {props.isAuth ? <NavigationItem link="/student">RegisterBooks</NavigationItem> : null}
-        {props.isAuth ? <NavigationItem link="/books">BooksSearch</NavigationItem> : null}
-        {props.isAuth ? <NavigationItem link="/logout" onLogout={props.onLogout}>Logout</NavigationItem> : <NavigationItem link="/signup">Signup</NavigationItem>}
-    </ul>
-);
+const navigationItems = (props) => {
+    return (
+        <ul className={classes.NavigationItems}>
+            {props.isAuth && props.isAdmin ? <NavigationItem link="/" exact>IssueBooks</NavigationItem> : null}
+            
+            {props.isAuth && !props.isAdmin ? <NavigationItem link="/" exact>Student Profile</NavigationItem> : null}
+
+            {props.isAuth && props.isAdmin ? <NavigationItem link="/student">RegisterBooks</NavigationItem> : null}
+            {props.isAuth && props.isAdmin ? <NavigationItem link="/books">BooksSearch</NavigationItem> : null}
+            {props.isAuth && props.isAdmin ? <NavigationItem link="/admin">CreateAdmin</NavigationItem> : null}
+            {props.isAuth ? <NavigationItem link="/logout" onLogout={props.onLogout}>Logout</NavigationItem> : <NavigationItem link="/signup">Signup</NavigationItem>}
+            {props.isAuth ? null : <NavigationItem link="/" exact>Login</NavigationItem>}
+        </ul>
+    );
+}
 
 export default navigationItems;
