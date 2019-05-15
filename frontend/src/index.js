cdd1c3bd84d85dfdd3d65eedfa4cb6b55d3ce54e
Rohit Kumawat <rkumawat125@gmail.com>
2019-05-15 06:19:10 +0530

    store connected to react app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './hoc/App';
 import * as serviceWorker from './serviceWorker';
+import reducer from './store/reducer';
+
+const store = createStore(reducer);
 
 const app = (
-    <BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
         <App />
     </BrowserRouter>
+    </Provider>
 )
 
 ReactDOM.render(app, document.getElementById('root'));
